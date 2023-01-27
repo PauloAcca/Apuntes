6ac263d4b9e92fe0868fbc75e2208497d5f8028b
PauloAcc <paulo.matias.accardo@gmail.com>
2023-01-27 14:30:35 -0300

    agrego modulo 4

---
+++ Modulo 4/clase 16/src/app.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname,"../public")))
+app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
+app.listen(3030, ()=>console.log("Servidor corriendo en el puerto 3030"));
